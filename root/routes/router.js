0fafa47fe688b0e46393085c381e140d8df351d4
Isaac Vieira <isaacfiuzavieira@gmail.com>
2023-10-27 08:15:50 -0300

    feat: rota da bored api

--- root/routes/router.js
+++ root/routes/router.js
@@ -1,14 +1,41 @@
-const express    = require('express')
-const router     = express.Router()
-const axios      = require('axios')
-const bodyParser = require('body-parser')
+const express = require('express')
+const router  = express.Router()
+const axios   = require('axios')
+const uuid    = require('uuid')
 
-require('dotenv').config();
+//Rota padrão ------------------------
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
     res.status(200).send({
         mensagem: 'Este é o app do Grupo 3 😃'
     });
 });
 
+//Rota /api/atividade ---------------------
+
+router.get('/api/atividades', async (req, res) => {
+
+    try{
+        const response = await axios.get('https://www.boredapi.com/api/activity')
+
+        const data = {
+
+        id: uuid.v4(),
+        atividade: response.data.activity,
+        tipo: response.data.type,
+        participantes: response.data.participants,
+        acessibilidade: response.data.accessibility*100 + '%',
+        }
+
+        res.status(200).send(data)
+        
+    }catch(error){
+
+        console.error('Erro ao buscar os dados da API:', error);
+        res.status(500).send('Erro ao buscar os dados da API');
+    }
+});
+
+
 module.exports = router
+
