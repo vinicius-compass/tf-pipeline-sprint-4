e3b5829bb8f9bea6e5bb85f0acf67aad519da43f
Vinicius Baggio <vinicius.baggio.pb@compasso.com.br>
2023-10-30 18:48:06 -0300

    refactor: jokeModel to conform to business logic

--- src/models/jokeModel.js
+++ src/models/jokeModel.js
@@ -1,26 +1,61 @@
 const axios = require("axios");
+const newUUID = require("../utils/UUID.js");
+
+/**
+ * @typedef {Object} Joke
+ * @property {string[]} categories Array of categories
+ * @property {string} created_at YYYY-MM-DD HH:MM:SS.SSSZ
+ * @property {string} icon_url URL to icon
+ * @property {string} id
+ * @property {string} updated_at YYYY-MM-DD HH:MM:SS.SSSZ
+ * @property {string} url URL to joke
+ * @property {string} value Joke string
+ * */
+
+/**
+ * @typedef {Object} FormatedJoke
+ * @property {string} data_atualizacao DD-MM-YYYY
+ * @property {string} data_criacao DD-MM-YYYY
+ * @property {string} icone URL to icon
+ * @property {string} id Random RFC 4122 version 4 UUID
+ * @property {string} piada Joke string
+ * @property {string} referencia URL to joke
+ * */
 
 const jokeModel = {
-    getJoke: async () => {
+    /**
+     * @param {string} date YYYY-MM-DD HH:MM:SS.SSSZ
+     * @returns {string} DD-MM-YYYY
+     * */
+    formatDate(date) {
+        const [year, month, day] = date.split(" ")[0].split("-");
+        return `${day}-${month}-${year}`;
+    },
+
+    /**
+     * @param {Joke} data
+     * @returns {FormatedJoke}
+     * */
+    formatData(data) {
+        return {
+            data_atualizacao: this.formatDate(data.updated_at),
+            data_criacao: this.formatDate(data.created_at),
+            icone: data.icon_url,
+            id: newUUID(),
+            piada: data.value,
+            referencia: data.url,
+        };
+    },
+
+    /**
+     * @returns {Promise<FormatedJoke>}
+     * */
+    async getJoke() {
         const response = await axios.get(
             "https://api.chucknorris.io/jokes/random",
         );
-        console.log(response.data);
-        const formatedData = formatData(response.data);
-        return formatedData;
+        return this.formatData(response.data);
     },
 };
-
-function formatData(data) {
-    const formatedData = {
-        data_atualizacao: data.updated_at,
-        data_criacao: data.created_at,
-        icone: data.icon_url,
-        id: data.id,
-        piada: data.value,
-        referencia: data.url,
-    };
-    return formatedData;
-}
 
 module.exports = jokeModel;
