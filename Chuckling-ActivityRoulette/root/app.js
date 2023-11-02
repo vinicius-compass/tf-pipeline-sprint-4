7c0b2fa8d1341d0ff373a17cfcfe1d813e3f1e17
Nadia Yuzawa <nadia.yuzawa.pb@compasso.com.br>
2023-11-02 19:20:50 -0300

    fix: mudança na porta

--- Chuckling-ActivityRoulette/root/app.js
+++ Chuckling-ActivityRoulette/root/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const routes = require('./routes/routes');
 
